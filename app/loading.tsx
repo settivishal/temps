56345005e99f1dce4bd12b32649b8b38db30edcc
Vishal K Setti <vishalsetti2000@gmail.com>
2026-02-10 11:14:22 -0500

    project setup delete

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="h-screen flex justify-center items-center bg-gray-100">
-      <p className="text-xl font-semibold">Loading...</p>
-    </div>
-  );
-}
